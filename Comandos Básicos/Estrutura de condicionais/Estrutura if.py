7dcafcf29d7fd80b81083baeb9513e81335f9a4d
DanielLemeez <cl.danielricardo@gamil.comm>
2024-10-28 13:37:42 -0300

    desvios

--- Comandos Básicos/Estrutura de condicionais/Estrutura if.py
+++ Comandos Básicos/Estrutura de condicionais/Estrutura if.py
@@ -2,5 +2,6 @@ tempoCarro=int(input("Quantos anos tem seu carro? "))
 # o 'if' significa 'se' após isso você coloca a condição, exemplo se Tempo do carro for menor que 3, imprima carro novo.
 if tempoCarro<=3:
     print("Seu carro não é velho.")
+# o 'else' significa basicamente 'se não'. Segue a mesma regra do 'if'.
 else:
     print("Seu carro é velho.")
